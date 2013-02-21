a15ed8ba9b296ab573eb6b0661871df52f27d231
Alex_Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2013-02-20 23:32:58 -0600

    Added packet maker and listen and syn recv

--- tcp_module.cc
+++ tcp_module.cc
@@ -133,7 +133,7 @@ void muxHandler(const MinetHandle &mux, const MinetHandle &sock, ConnectionList<
 	tcph.GetWinSize(winsize);
 	tcph.GetFlags(flags);
 
-	 ConnectionList<TCPState>::iterator cs=clist.FindMatching(c);
+	 ConnectionList<TCPState>::iterator cs = clist.FindMatching(c);
 
 	 if (cs!=clist.end()) {
 	   iph.GetTotalLength(len);
@@ -165,12 +165,12 @@ void muxHandler(const MinetHandle &mux, const MinetHandle &sock, ConnectionList<
 		    (*cs).state.SetState(SYN_RCVD);  //set the state to SYN_RCVD because we just received a SYN  (RFC)
 		    (*cs).bTmrActive = true;   //turn on the timer
 		    (*cs).connection = c;   //reset the connection
-		    //since we have the sequence number, we can set it as the last received (incremented by one)
-		    (*cs).state.SetLastRecvd(seq+1);
+		    //since we have the sequence number, we can set it as the last received
+		    (*cs).state.SetLastRecvd(seqnum);
 		    (*cs).timeout = Time() + 10; //I dunno why 10 would be good or not...
-		    (*cs).state.last_acked = (*cs).state.last_sent - 1;
 		    
 		    //create the SYN/ACK packet   (RFC)  --> out_packet comes from this
+		    packetMaker(out_packet, *cs, 0, 3);  //3 does SYN/ACK
 		    MinetSend(mux, out_packet);
 		}
 		else if (IS_FIN(flags)) {
@@ -182,12 +182,11 @@ void muxHandler(const MinetHandle &mux, const MinetHandle &sock, ConnectionList<
 	     // handle state SYN_RCVD;
 	     printf("syn_rcvd\n");
 	     if (IS_ACK(flags)) {
-		//.GetLastSent() will get the last sent ack, but we want to increment by one to see if it matches with what we expect our ack to be due to sequential acknowledgement
-		if ((*cs).state.GetLastSent() + 1 == ack) {
+		if ((*cs).state.GetLastAcked() == seqnum) {
 		    printf("Established\n");
 		    (*cs).state.SetState(ESTABLISHED);  //since we got here that means we are established!
 		    (*cs).state.SetSendRwnd(winsize);  //set the window size
-		    (*cs).state.SetLastAcked(ack);  //reset the last recieved ack
+		    (*cs).state.SetLastAcked(acknum);  //reset the last recieved ack
 		    (*cs).bTmrActive = false;  //we recieved the packet so turn off the timer!
 
 		    //Now we need to talk with the socket...
@@ -202,6 +201,16 @@ void muxHandler(const MinetHandle &mux, const MinetHandle &sock, ConnectionList<
 	     break;
 	   case SYN_SENT:
 	     //handle state SYN_SENT;
+	     	printf("syn_sent\n");
+	     	if (IS_SYN(flags) && IS_ACK(flags))
+	     	{
+		    (*cs).state.SetSendRwnd(winsize);  //set the send window 
+		    (*cs).state.SetLastRecvd(seqnum + 1);
+		    //send an ACK packet
+		    //Then send to MinetSend
+		    MinetSend(mux, out_packet);
+		    (*cs).state.SetState(ESTABLISHED);
+		}
 	     break;
 	   case SYN_SENT1:
 	     //handle state SYN_SENT1;
@@ -240,6 +249,69 @@ void muxHandler(const MinetHandle &mux, const MinetHandle &sock, ConnectionList<
 >>>>>>> 93db8eeeaa6bf5aa211711ed10394effc91e6d60
 
 	cerr << "Checksum is " << (tcph.IsCorrectChecksum(p) ? "VALID" : "INVALID");
+}
+
+void packetMaker(Packet &packet, ConnectionToStateMapping<TCPState>& constate, int size_of_data, int whichFlag)
+{
+
+  unsigned int flags = 0;
+
+  int packet_length = size_of_data + TCP_HEADER_BASE_LENGTH + IP_HEADER_BASE_LENGTH;
+  IPHeader ip;
+  TCPHeader tcp;
+  IPAddress source = constate.connection.src;
+  IPAddress destination = constate.connection.dest;
+
+  //create the IP packet with source, destination, length, and protocol
+  ip.SetSourceIP(source);
+  ip.SetDestIP(destination);
+  ip.SetTotalLength(packet_length);
+  ip.SetProtocol(IP_PROTO_TCP);
+
+  packet.PushFrontHeader(ip);
+
+  switch (whichFlag){
+  case 1:
+    SET_SYN(flags);
+    break;
+  case 2:
+    SET_ACK(flags);
+    break;
+  case LISTEN:
+    SET_ACK(flags);
+    SET_SYN(flags);
+    break;
+  case 4:
+    SET_PSH(flags);
+    SET_ACK(flags);
+    break;
+  case 5:
+    SET_FIN(flags);
+    break;
+  case 6:
+    SET_FIN(flags);
+    SET_ACK(flags);
+    break;
+  case 7:
+    SET_RST(flags);
+    break;
+  default:
+    break;
+  }
+
+  //just followed all the tcp.h stuff top to bottom
+
+  tcp.SetSourcePort(constate.connection.srcport, packet); 
+  tcp.SetDestPort(constate.connection.destport, packet);   
+  tcp.SetHeaderLen(TCP_HEADER_BASE_LENGTH, packet);  
+  tcp.SetFlags(0, packet);  
+  tcp.SetAckNum(constate.state.GetLastRecvd(), packet);
+  tcp.SetSeqNum(constate.state.GetLastAcked()+1, packet);
+  tcp.SetWinSize(constate.state.GetN(), packet);
+  tcp.SetUrgentPtr(0, packet);
+  tcp.RecomputeChecksum(packet);
+  packet.PushBackHeader(tcp);
+  
 }
 
 
